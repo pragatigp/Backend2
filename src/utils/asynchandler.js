83b17acf6cb2dcf0644ce2db30e0ac6dd3eb88d4
pragatigp <pragatigp620@gmail.com>
2026-03-01 12:17:25 +0530

    util code discussion

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,26 @@
+const asynchandler=(requestHandler)=>{
+  (req,res,next)=>{
+    Promise.resolve(requestHandler(req.res.next)).catch((err)=>next(err))
+  }
+}
+
+
+
+
+
+
+
+
+export {asynchandler}
+
+// const asynchandler=(fn)=>async(req,res,next)=>{
+//   try{
+//   await fn(req,res,next)
+//   }catch(error){
+//     res.status(err.code  || 500).json({
+//       succcess:false,
+//       message:err.message
+//     })
+//   }
+  
+// }
