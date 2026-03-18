c589c837ebad07dad8694b49939cab3f532900f5
pragatigp <pragatigp620@gmail.com>
2026-03-18 15:05:36 +0530

    Add refreshToken endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,23 @@ import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { Apiresponse } from "../utils/Apiresponse.js";
+import jwt from "jsonwebtoken"
+
+
+const generateAccessAndRefreshTokens = async(userId)=>{
+  try{
+    const user=await User.findById(userId)
+    const accessToken=user.generateAccessToken()
+    const refreshToken=user.generateRefreshToken()
+
+    user.refreshToken=refreshToken
+    await user.save({validateBeforeSave:false})
+    return {accessToken,refreshToken}
+  }catch(error){
+    throw new ApiError(500, error?.message || "Something wnet wrong while generating refresh  and access token")
+  }
+  
+}
 
 const registerUser=asynchandler( async(req,res)=>{
  //  get user details from frontend
@@ -78,4 +95,128 @@ return res.status(201).json(
 )
 })
 
-export {registerUser}
+const loginUser=asynchandler(async(req,res)=>{
+    //req body->data
+    // username or email
+    //find the user
+    // password is correect
+    //access and refresh token is generated 
+    //send cookies
+    
+
+    const {email,userName,password}=req.body
+    console.log(email);
+
+    if(!userName && !email){
+      throw new ApiError(400,"username or email is required")
+    }
+
+    const user=await User.findOne({
+      $or : [{userName},{email}]
+    })
+
+    if(!user){
+      throw new ApiError(404,"User does not exist")
+    }
+
+    const isPasswordValid = await  user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+      throw new ApiError(401,"Invalid user credentials")
+    }
+
+    const {accessToken,refreshToken}=await generateAccessAndRefreshTokens(user._id)
+    
+    const loggedInUser=await User.findById(user._id).select("-password -refreshToken")
+
+    const options={
+      httpOnly:true,
+      secure:true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+      new Apiresponse(
+        200,
+        {
+          user:loggedInUser,accessToken,refreshToken
+        },
+        "User logged is successfully"
+
+      )
+    )
+})
+
+
+const logoutUser=asynchandler(async(req,res)=>{
+   await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken:undefined
+      }
+    },
+    {
+      new:true
+    }
+   )
+   const options={
+      httpOnly:true,
+      secure:true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new Apiresponse(200,{}," User logged out "))
+
+})
+
+const refreshAccessToken = asynchandler(async(req,res)=>{
+  const incomingRefreshToken= req.cookies.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401,"Unauthorized request")
+  }
+
+  try {
+    const  decodedToken= jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+  
+    const user= await User.findById(decodedToken?._id)
+    if(!user){
+      throw new ApiError(401,"Invalid refresh token")
+    }
+    if(incomingRefreshToken !== user?.refreshToken){
+       throw new ApiError(401,"Refresh token is expired or used ")
+    }
+  
+    const options={
+      httpOnly:true,
+      secure:true
+    }
+    const {accessToken,newrefreshToken}=await generateAccessAndRefreshTokens(user_.id)
+  
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken,options)
+     .cookie("refreshToken",newrefreshToken,options)
+   .json(
+    new Apiresponse(
+      200,
+      {accessToken,newrefreshToken:newrefreshToken},"Access token refreshed"
+    )
+   )
+  } catch (error) { 
+    throw new ApiError(401,error?.message || "Invalid refresh token")
+  }
+})
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken}
